5a3502a5e06e3be3c0ab3a6d99e042a91a2a9183
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-06-30 10:14:17 -0700

    adding more shape to onboarding process

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,38 +1,63 @@
 import React from 'react'
 import businessLogos from '../components/Carousel/businesslogos'
+
+import Box from "@material-ui/core/Box";
 import Funavatar from '../components/Funavatar';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import funLogos from '../components/Carousel/funlogos';
 import Button from "@material-ui/core/Button";
-import { TextField, Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { TextField, Grid, makeStyles, Typography } from '@material-ui/core';
 
 
 const useStyles = makeStyles((theme) => ({
+
+
+    contain: {
+        marginTop: `10vh`,
+        width: `65vw`,
+        justifyContent: "center",
+    },
     starter: {
         fontsize: '1.5rem',
-        fontfamily: 'Poppins'
+        fontfamily: 'Poppins',
+        justifyContent: 'center',
+        marginTop: `1vh`,
     },
     alterHeader: {
         fontsize: '1.5rem',
-        fontfamily: 'Poppins'
+        marginTop: `2vh`,
+        weight: `600`
     },
     alterIntro: {
         fontsize: '1.5rem',
-        fontfamily: 'Poppins'
+        fontfamily: 'Poppins',
+        margin: `1vh`
     },
     joinbutton: {
-        color: "white",
-        background: `blue`,
-        fontSize: '1.5rem',
-        margin: `2vh`,
-        fontWeight: 400,
-        "&:hover": {
-            color: `green`
-        }
+        lineheight: '50px',
+        height: '50px',
+        textalign: 'center',
+        width: '250px',
+        cursor: 'pointer',
+        backgroundColor: 'purple',
+        color: 'blue',
+        transition: 'all 0.3s',
+        position: 'center',
     },
+    span: {
+        transition: 'all 0.3s',
+    },
+    logopick: {
+        width: '100vw'
+    }
+,
+
     field: {
-        display: 'flex'
+        display: 'flex',
+        margin: `1vh`,
+     
     }
 }));
 
@@ -46,27 +71,53 @@ const Home = () => {
 
     return (
         <div>  
+            <Box className={classes.contain}>
             <Grid>
-            <Grid>
-                <Typography className={classes.starter}> Let's get you connected!</Typography>  </Grid>
+                <Typography className={classes.starter} > Get Connected</Typography>  </Grid>
 
             <Grid><Typography className={classes.alterHeader}>Avatar Alter Egos</Typography></Grid>
 
             <Grid> <Typography className={classes.alterIntro}>Pick a fave tech:</Typography></Grid>
-
-            <Funavatar avatars={avatars} />
+            <Funavatar avatars={avatars} /> 
 
             <Grid> <Typography className={classes.alterIntro}>If you're gonna party, who is it with?</Typography></Grid>
            
-            <Funavatar avatars={funLogos} />
-            
+            <Funavatar className={classes.logopick} avatars={funLogos} />
+            <Typography className={classes.alterHeader}>Social Profiles</Typography>
             <form className={classes.root} noValidate autoComplete="off">
                 <Grid>
-                    <TextField className={classes.field} id="outlined-basic" label="LinkedIn" variant="outlined" />   </Grid>
-                <Grid> <TextField className={classes.field} id="outlined-basic" label="Instagram" variant="outlined" />   </Grid>
-                <Grid><Button className={classes.joinbutton}>Connect</Button></Grid>
-            </form>
-        </Grid>
+
+                <TextField
+        className={classes.field} variant="outlined" 
+        id="input-with-icon-textfield"
+        label="LinkedIn Profile"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <LinkedInIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+        <TextField
+        className={classes.field} variant="outlined" 
+        id="input-with-icon-textfield"
+        label="Instragram Profile"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <InstagramIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+
+</Grid></form>
+
+                    
+                <Grid><Button className={classes.joinbutton}><span>Connect</span></Button></Grid>
+        
+        </Box>
         </div>
     )
 }
