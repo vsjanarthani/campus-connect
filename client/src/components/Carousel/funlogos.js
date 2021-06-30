8be30fcaae9ace41091750ca8c64f1b7627f3c6f
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-06-29 20:19:18 -0700

    themeswitcher avatar assets

---
+++ client/src/components/Carousel/funlogos.js
@@ -0,0 +1,60 @@
+
+const FunLogos = {
+
+    logos:[
+    {
+        id: `1`,
+        title: `carebear`,
+        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022845/Frame_9_rta6qr.png`,
+
+      
+    },
+
+    {id: `2`,
+        title: `pikachu`,
+        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022844/Frame_7_vfulja.png`,
+
+    },
+
+    {id: `3`,
+        title: `dexter`,
+        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022843/dexter_gfisah.png`,
+
+      
+    },
+    {id: `4`,
+        title: `bojack`,
+        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022843/Frame_8_khurfl.png`,
+    
+    },
+  
+    {id: `5`,
+        title: `roger`,
+        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022844/roger_g7wx8q.png`,
+   
+       
+    },
+
+    { id: `6`,
+        title: `ninjaturtle`,
+        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022844/Frame_5_jpasit.png`,
+  
+    },
+
+    { id: `7`,
+        title: `streetshark`,
+        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022843/streetsharks_srdjpa.png`,
+  
+    },
+
+    { id: `8`,
+        title: `homer`,
+        image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022844/Frame_6_as3y2p.png`,
+  
+    }
+
+   
+]}
+
+
+export default FunLogos
