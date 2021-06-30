31f242936ef246dc7cb72847e019c3e433eff47d
Graham Hall <ghall89@me.com>
2021-06-30 00:44:25 -0400

    color toggle test

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloProvider } from '@apollo/client';
 import ApolloClient from 'apollo-boost';
@@ -11,35 +11,63 @@ import Signup from './pages/Signup/Signup';
 import NoMatch from './pages/NoMatch';
 import Chat from './pages/Chat/Chat';
 
-const client = new ApolloClient({
-  request: operation => {
-    const token = localStorage.getItem('id_token');
+// Material UI
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    });
-  },
-  uri: 'http://localhost:3001/graphql'
+const client = new ApolloClient({
+	request: operation => {
+		const token = localStorage.getItem('id_token');
+
+		operation.setContext({
+			headers: {
+				authorization: token ? `Bearer ${token}` : ''
+			}
+		});
+	},
+	uri: 'http://localhost:3001/graphql'
 });
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Header />
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/chat" component={Chat} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Router>
-      <Footer />
-    </ApolloProvider>
-  );
+	const [theme, setTheme] = useState('blue');
+
+	const toggle = () => {
+		if (theme === 'blue') {
+			setTheme('green');
+		} else if (theme === 'green') {
+			setTheme('blue');
+		}
+	};
+
+	const useStyles = makeStyles({
+		root: {
+			backgroundColor: theme,
+			height: '30px',
+			width: '30px'
+		}
+	});
+
+	const classes = useStyles();
+
+	return (
+		<ApolloProvider client={client}>
+			<Header />
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/signup" component={Signup} />
+					<Route exact path="/chat" component={Chat} />
+					<Route component={NoMatch} />
+				</Switch>
+			</Router>
+			<Button onClick={toggle} variant="contained">
+				Click me!
+			</Button>
+			<div className={classes.root}></div>
+			<Footer />
+		</ApolloProvider>
+	);
 }
 
 export default App;
