42781da555be34de2f3097eaf8afa665c338f05f
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-07-01 17:03:15 -0700

    add assets for logo/avatar selection

---
+++ client/src/components/Carousel/funlogos.js
@@ -0,0 +1,75 @@
+
+const funLogos =
+
+    [
+        {
+            id: `1`,
+            title: `carebear`,
+            image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022845/Frame_9_rta6qr.png`,
+            nameStyle: `funlogo`,
+            tip: `I dare you to say something bad about carebears.`
+
+        },
+
+        {
+            id: `2`,
+            title: `pikachu`,
+            image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022844/Frame_7_vfulja.png`,
+            nameStyle: `funlogo`,
+            tip: `Manny would totally pick this, then hate on Pikachu.`
+        },
+
+        {
+            id: `3`,
+            title: `dexter`,
+            image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022843/dexter_gfisah.png`,
+            nameStyle: `funlogo`,
+            tip: ` There is gloom, and doom while things go boom in Dexter's Lab!`
+
+        },
+        {
+            id: `4`,
+            title: `bojack`,
+            image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022843/Frame_8_khurfl.png`,
+            nameStyle: `funlogo`,
+            tip:`When You Look At Someone Through Rose-Colored Glasses, All The Red Flags Just Look Like Flags.”`
+        },
+
+        {
+            id: `5`,
+            title: `roger`,
+            image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022844/roger_g7wx8q.png`,
+            nameStyle: `funlogo`,
+            tip: `Screw You, I’m Kevin Bacon!`
+
+        },
+
+        {
+            id: `6`,
+            title: `ninjaturtle`,
+            image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022844/Frame_5_jpasit.png`,
+            nameStyle: `funlogo`,
+            tip: `Cowabunga!`
+        },
+
+        {
+            id: `7`,
+            title: `streetshark`,
+            image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022843/streetsharks_srdjpa.png`,
+            nameStyle: `funlogo`,
+            tip: `Jawesome!`
+        },
+
+        {
+            id: `8`,
+            title: `homer`,
+            image: `https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022844/Frame_6_as3y2p.png`,
+            nameStyle: `funlogo`,
+            tip: `Trying is the first step toward failure.`
+        }
+
+
+    ]
+
+
+export default funLogos
