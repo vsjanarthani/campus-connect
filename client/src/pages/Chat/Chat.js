679da5a70ed72b85d7e1c618aeb06fc9cbae846f
MeganClose <megs.close@gmail.com>
2021-07-02 09:11:01 -0700

    trying to make sure to bring in needed chat features on new chat page

--- client/src/pages/Chat/Chat.js
+++ client/src/pages/Chat/Chat.js
@@ -24,20 +24,65 @@ import Online from "../../components/Online/Online"
 
 const Chat = () => {
 
-    const classes = useStyles();
+    // const classes = useStyles();
+
+    //     return (
+    //         <MessageProvider>
+    //             <Paper className={classes.root}>
+    //                 <Grid container spacing={2}>
+    //                     <Grid item>
+    //                         <UserList />
+    //                     </Grid>
+    //                     <Grid item>
+    //                         <ChatBody />
+    //                     </Grid>
+    //                 </Grid>
+    //             </Paper>
+    //         </MessageProvider>
+    //     )
+    // }
 
     return (
         <MessageProvider>
-            <Paper className={classes.root}>
-                <Grid container spacing={2}>
-                    <Grid item>
-                        <UserList />
-                    </Grid>
-                    <Grid item>
-                        <ChatBody />
-                    </Grid>
-                </Grid>
-            </Paper>
+            <div className="messenger">
+                <div className="chatMenu">
+                    <div className="chatMenuWrapper">
+                        <input className="chatMenuInput" placeholder="Search for Friends" type="text" />
+                        <Conversation />
+                        <Conversation />
+                        <Conversation />
+                        <Conversation />
+                    </div>
+                </div>
+                <div className="chatBox">
+                    <div className="chatBoxWrapper">
+                        <div className="messagesHere">
+                            <Message />
+                            <Message user={true} />
+                            <Message />
+                            <Message />
+                            <Message user={true} />
+                            <Message user={true} />
+                            <Message user={true} />
+                            <Message user={true} />
+                            <Message user={true} />
+                            <Message user={true} />
+                            <Message user={true} />
+                            <Message user={true} />
+                            <Message user={true} />
+                        </div>
+                        <div className="chatBoxBottom">
+                            <textarea className="messageInput" placeholder="Write Your Message"></textarea>
+                            <button className="submit">Send</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="chatOnline">
+                    <div className="chatOnline">
+                        <Online />
+                    </div>
+                </div>
+            </div>
         </MessageProvider>
     )
 }
