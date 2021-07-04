e15bccb06e7a5ddb4eb667d38606d80e03e4c886
sarahdurks <77648727+sarahdurks@users.noreply.github.com>
2021-07-03 21:23:54 -0700

    addition of friend menu for mobile  - media queries/show hide incomplete

--- client/src/components/MobileRail/index.js
+++ client/src/components/MobileRail/index.js
@@ -1,19 +1,18 @@
 import React from "react";
 //Material UI Items
 import Divider from "@material-ui/core/Divider";
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import UserList from '../../components/UserList/UserList';
-import { MessageProvider } from '../../utils/messagecontext';
+// import { MessageProvider } from '../../utils/messagecontext';
 
-import Chatter from "../../components/ChatBody/ChatSubmit";
-import ForumIcon from '@material-ui/icons/Forum';
-import Send from '@material-ui/icons/Send';
-import SearchIcon from '@material-ui/icons/Search';
+// import ForumIcon from '@material-ui/icons/Forum';
+// import Send from '@material-ui/icons/Send';
+// import SearchIcon from '@material-ui/icons/Search';
 import Avatar from '@material-ui/core/Avatar';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import TextField from "@material-ui/core/TextField";
+// import InputAdornment from '@material-ui/core/InputAdornment';
+// import TextField from "@material-ui/core/TextField";
 import Typography from '@material-ui/core/Typography';
 import Grid from "@material-ui/core/Grid";
 const useStyles = makeStyles(() => ({
@@ -78,7 +77,9 @@ const Rail = ({ navigationLinks }) => {
     return (
         <Box className={classes.menuRail} component="div">
         <Divider />      <Divider /><Grid className="myname" >  
-              <Typography className={classes.railname}> <Avatar alt= "https://res.cloudinary.com/janarthani/image/upload/v1620088367/007_ooqqgu.png" />Username Here</Typography>
+              <Typography className={classes.railname}> <Avatar id="myavatar" src="https://res.cloudinary.com/www-actionnetwork-com/image/upload/v1625022844/Frame_5_jpasit.png" style={{
+             border: '0.1px solid lightgray'
+          }}></Avatar> <span id="namename">[ADD NAME VARIABLE]'s Friends</span></Typography>
               </Grid> <Divider /><Divider />
            
             <div className="messenger">
