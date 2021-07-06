ad7699ddd3e60caaf86720ffca0d912a0a20db0b
Clayton Goff <cltngoff@gmail.com>
2021-07-06 00:57:23 -0700

    redirect to onboard from login

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -115,7 +115,7 @@ const Login = () => {
         },
         onCompleted(data) {
             dispatch({ type: 'LOGIN', payload: data.login })
-            window.location.href = '/chat'
+            window.location.href = '/onboard'
         },
     })
 
