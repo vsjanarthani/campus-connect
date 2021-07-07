c8f75842d9c9c43adbaa7f482e7a8478fb1fcccd
MeganClose <megs.close@gmail.com>
2021-07-06 17:29:07 -0700

    onboard to home

--- client/src/pages/Chat/Chat.js
+++ client/src/pages/Chat/Chat.js
@@ -28,13 +28,9 @@ import {
 import { isNonEmptyArray } from '@apollo/client/utilities';
 
 const Chat = props => {
-<<<<<<< HEAD
-	const useStyles = makeStyles(theme => ({
-=======
 
 	const useStyles = makeStyles(theme => ({
 
->>>>>>> 39c9c853064ea8d079d3de9d674284a802144b31
 		root: {
 			dividerColor: `#F5F5F5`
 		},
