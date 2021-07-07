256a164bf65591504edff93a5d732699950a9121
Janarthani V Subramaniam <vs.janarthani@gmail.com>
2021-07-07 01:15:57 -0700

    changes after revert

---
+++ client/src/components/ChatBody/chatInitial.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+// const Initial = () => {
+
+//     return (
+
+//         <div>
+//             <Image src="https://res.cloudinary.com/actionfdfds/image/upload/v1625623097/Frame_23_kqm48f.png" />
+//         </div>
+
+//     )
+// }
+// export default Initial
