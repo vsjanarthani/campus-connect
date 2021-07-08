74e0543793c61fd7582dbbe7f29e3adb0fc8e756
Graham Hall <ghall89@me.com>
2021-07-07 21:53:36 -0400

    appreciation messages

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,42 +1,67 @@
 import React from 'react';
 import './animations.css';
 import { useAuthState } from '../../utils/auth';
-import { Typography, Container } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { Typography, Container } from '@material-ui/core';
+import appreciationData from './appreciationData';
+import Appreciate from './card';
 
 const Home = props => {
-	// 	const useStyles = makeStyles((theme) => {
-	// 		celebrate: {
-	// 			marginTop: '50%'
-	// 		}
-	// 	});
-	//
-	// 	const classes = useStyles();
+	const useStyles = makeStyles(theme => ({
+		celebrate: {
+			marginTop: '50%'
+		},
+		root: {
+			flexGrow: 1,
+			width: '100%',
+			display: 'flex',
+			justifyContent: 'center',
+			flexWrap: 'wrap'
+		},
+		slide: {
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			width: '40%'
+		}
+	}));
+
+	const classes = useStyles();
 
 	const { user } = useAuthState();
 	console.log(user);
+	const appreciations = appreciationData.appreciation;
 	return (
 		<div>
 			{!user ? (
 				<>
-					<Typography>Welcome to Campus Connect</Typography>
 					<Typography variant="h2" className="bounce-in">
-						Thank you!!
+						Its Graduation Day!!
 					</Typography>
 					<Container maxWidth="sm">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iusto
-						error voluptatem eius accusantium, fugiat rerum provident alias modi
-						officiis nostrum, velit commodi sint deserunt reprehenderit illo
-						odit culpa quibusdam?
+						Congratulations to all of you for successfully completing the coding
+						bootcamp. It was a challenging journey for all of us. Sign up and
+						join campus connect to stay in touch with cohort and TAs.
 					</Container>
 				</>
 			) : (
 				<>
 					{' '}
-					<Typography>Welcome {user.data.username}</Typography>{' '}
-					<Typography variant="h2" className="bounce-in">
-						Congrats {user.data.username}!
-					</Typography>
+					{/* <Typography>Welcome {user.data.username}</Typography>{' '} */}
+					<div className={classes.root}>
+						{appreciations.map(appreciation => {
+							return (
+								<div className={classes.slide} key={appreciation.title}>
+									<Appreciate
+										imageUrl={appreciation.image}
+										to={appreciation.to}
+										message={appreciation.message}
+										from={appreciation.from}
+									/>
+								</div>
+							);
+						})}
+					</div>
 				</>
 			)}
 		</div>
